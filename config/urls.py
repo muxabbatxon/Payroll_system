9c82c146504c82c5cbc029fbdb54800bc8de69a1
muxabbatxon <gofurjonovamuxabbatxon4@gmail.com>
2026-08-20 16:18:27 +0500

    Tugadiiiiiiiiii

--- config/urls.py
+++ config/urls.py
@@ -4,22 +4,12 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from accounts.views import EmailTokenObtainPairView
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import get_user_model
-from django.http import HttpResponse
 
-def create_admin_quick(request):
-    User = get_user_model()
-    email = "admin@example.com"
-    if not User.objects.filter(email=email).exists():
-        User.objects.create_superuser(email=email, password="adminpassword123")
-        return HttpResponse("Muvaffaqiyatli! Admin yaratildi: admin@example.com / adminpassword123")
-    return HttpResponse("Bu admin allaqachon mavjud!")
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('make-admin-now/', create_admin_quick),
     path('api/auth/login/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
